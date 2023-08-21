485c7a92b33a5076e802b27b10a6745ed362ef4e
bagzero <135484695+bagzero@users.noreply.github.com>
2023-08-21 03:39:30 -0400

    Delete app.js

--- app.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const loggedInUsername = localStorage.getItem("loggedInUsername");
-  
-    if (loggedInUsername) {
-      const usernameDisplay = document.getElementById("usernameDisplay");
-      usernameDisplay.textContent = loggedInUsername;
-    }
-  });
